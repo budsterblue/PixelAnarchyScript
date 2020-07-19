bb68c689c58fda2d657f159847d4639da3ce0109
Budsterblue <budsterblue@gmail.com>
2020-07-19 06:46:26 -0500

    many fixes/tweaks and new implementation of sidebar toggle adapted from bs2k's script

--- PixelAnarchyOverlay.user.js
+++ PixelAnarchyOverlay.user.js
@@ -7,7 +7,7 @@
 // @include        https://pixelanarchy.online/*
 // @match          http://pixelanarchy.online/*
 // @match          https://pixelanarchy.online/*
-// @version        1.5.1
+// @version        1.6
 // ==/UserScript==
 
 
@@ -23,29 +23,37 @@ pxSummary.innerText = "Budsterblue's Options";
 pxOptions.appendChild(pxSummary);
 pxSidebar.appendChild(pxOptions);
 var pxOverlay = document.getElementById("overlay");
-pxOverlay.crossOrigin = "anonymous";
+var pxPallete = document.getElementsByClassName('pallete')[0];
+
+// --Script Version--
+pxVersion = document.createElement("h3");
+pxVersion.innerHTML = "Script Version: " + GM.info.script.version;
+pxVersion.style.position = "absolute";
+pxVersion.style.bottom = 0;
+pxVersion.style.marginLeft = "5px";
+pxSidebar.appendChild(pxVersion);
 
 // --Exact Overlay Scaling--
-var pxWidthGroup = document.createElement("form-group");
-pxWidthGroup.style.display = "block";
+var pxAreaGroup = document.createElement("form-group");
+pxAreaGroup.style.display = "block";
+// Width
 pxWidth = document.createElement("input");
 pxWidth.type = "number";
 pxWidth.style.width = "60px";
+pxWidth.placeholder = "W";
 pxWidth.addEventListener('change', (event) => { pxOverlay.style.width = pxWidth.value + "px"; });
-var pxHeightGroup = document.createElement("form-group");
-pxHeightGroup.style.display = "block";
+// Height
 pxHeight = document.createElement("input");
 pxHeight.type = "number";
 pxHeight.style.width = "60px";
+pxHeight.style.marginLeft = "2px";
+pxHeight.placeholder = "H";
 pxHeight.addEventListener('change', (event) => { pxOverlay.style.height = pxHeight.value + "px"; });
 
-pxOptions.appendChild(document.createTextNode("\nOverlay Width & Height\n"));
-pxWidthGroup.appendChild(pxWidth);
-pxWidthGroup.appendChild(document.createTextNode("Width"));
-pxHeightGroup.appendChild(pxHeight);
-pxHeightGroup.appendChild(document.createTextNode("Height"));
-pxOptions.appendChild(pxWidthGroup);
-pxOptions.appendChild(pxHeightGroup);
+pxOptions.appendChild(document.createTextNode("Overlay Width & Height"));
+pxAreaGroup.appendChild(pxWidth);
+pxAreaGroup.appendChild(pxHeight);
+pxOptions.appendChild(pxAreaGroup);
 
 // --Message Expander--
 var pxMessagesGroup = document.createElement("form-group");
@@ -54,13 +62,14 @@ var pxMessages = document.getElementById("messages");
 var pxMessagesInput = document.createElement("input");
 pxMessagesInput.type = "number";
 pxMessagesInput.style.width = "60px";
+pxMessagesInput.style.marginTop = "5px";
 pxMessagesInput.addEventListener('change', (event) => { pxMessages.style.height = pxMessagesInput.value + "px"; });
 pxMessagesGroup.appendChild(pxMessagesInput);
 pxMessagesGroup.appendChild(document.createTextNode("Chat Height"));
 pxOptions.appendChild(pxMessagesGroup);
 
 // --Pixel Preview--
-var pxPreviewEnabled = false;
+//var pxPreviewEnabled = true;
 var pxPreviewGroup = document.createElement("form-group");
 pxPreviewGroup.style.display = "block";
 var pxPreview = document.createElement("input");
@@ -74,40 +83,36 @@ var pxCtx = pxPreviewCanvas.getContext('2d');
 pxCtx.strokeStyle = "gray";
 pxCtx.lineWidth = 2;
 pxCtx.fillStyle = "#FFFFFF";
+// First Run
+pxCanvas.style.cursor = "";
+pxCtx.moveTo(2,2);
+pxCtx.lineTo(18,20);
+pxCtx.lineTo(2,26);
+pxCtx.lineTo(2,1);
+pxCtx.stroke();
+pxCtx.fill();
+pxScale.style.cursor = 'url(' + pxPreviewCanvas.toDataURL() + '), auto';
+
 // Get Current Color (thanks bs2k for solving this part of the puzzle!), then fill a square and apply it to the mouse cursor
 [...document.getElementsByClassName('btnbelow')].forEach(function(elem){
     elem.addEventListener('click',function(e){
-    if (pxPreviewEnabled){
-        if (e.srcElement.id == "mix"){
-            var gradient = pxCtx.createLinearGradient(0, 0, 30, 0);
-            gradient.addColorStop(0, 'red');
-            gradient.addColorStop(1 / 6, 'orange');
-            gradient.addColorStop(2 / 6, 'yellow');
-            gradient.addColorStop(3 / 6, 'green');
-            gradient.addColorStop(4 / 6, 'blue');
-            gradient.addColorStop(5 / 6, 'indigo');
-            gradient.addColorStop(1, 'violet');
-            pxCtx.fillStyle = gradient;
-        } else{
-            pxCtx.fillStyle = e.srcElement.id;
-        }
-        pxCtx.moveTo(2,2);
-        pxCtx.lineTo(18,20);
-        pxCtx.lineTo(2,26);
-        pxCtx.lineTo(2,1);
-        pxCtx.stroke();
-        pxCtx.fill();
-        pxScale.style.cursor = 'url(' + pxPreviewCanvas.toDataURL() + '), auto';
-    
-        }
+	pxCtx.fillStyle = e.srcElement.id;
+    	if (pxPreview.checked) {
+        	pxCtx.moveTo(2,2);
+        	pxCtx.lineTo(18,20);
+        	pxCtx.lineTo(2,26);
+        	pxCtx.lineTo(2,1);
+        	pxCtx.stroke();
+        	pxCtx.fill();
+        	pxScale.style.cursor = 'url(' + pxPreviewCanvas.toDataURL() + '), auto'; 
+    	}
     })
 })
 
 // State
 pxPreview.addEventListener('change', (event) => {
   if (event.target.checked) {
-    document.getElementById("myCanvas").style.cursor = "";
-    pxPreviewEnabled = true;
+    pxCanvas.style.cursor = "";
     pxCtx.moveTo(2,2);
     pxCtx.lineTo(18,20);
     pxCtx.lineTo(2,26);
@@ -116,8 +121,7 @@ pxPreview.addEventListener('change', (event) => {
     pxCtx.fill();
     pxScale.style.cursor = 'url(' + pxPreviewCanvas.toDataURL() + '), auto';
   } else {
-    document.getElementById("myCanvas").style.cursor = "crosshair";
-    pxPreviewEnabled = false;
+    pxCanvas.style.cursor = "crosshair";
     pxScale.style.cursor = "pointer";
   }
 });
@@ -126,36 +130,72 @@ pxPreviewGroup.appendChild(pxPreview);
 pxPreviewGroup.appendChild(document.createTextNode("Toggle Preview"));
 pxOptions.appendChild(pxPreviewGroup);
 
-// --Sidebar Toggle--
-var pxSidebarOpen = true;
-var pxSidebarButton = document.createElement("h2");
-pxSidebarButton.innerText = "◄"
+// --Sidebar Toggle (adapted from bs2k's implementation)--
+// Style Additions
+pxSidebar.style.transition = "all 1s";
+pxPallete.style.transition = "all 1s";
+// Div
+pxSidebarButton = document.createElement("div");
+pxSidebarButton.style.width = "250px";
+pxSidebarButton.style.height = "50px";
+pxSidebarButton.style.top = "0";
+pxSidebarButton.style.left = "0";
+pxSidebarButton.style.bottom = "0";
+pxSidebarButton.style.transition = "all 1s";
+pxSidebarButton.style.overflow = "visible";
+pxSidebarButton.style.margin = "auto";
 pxSidebarButton.style.position = "absolute";
-pxSidebarButton.style.left = "0%";
-pxSidebarButton.style.bottom = "0%";
-pxSidebarButton.style.zIndex = "100";
-pxSidebarButton.style.color = "white";
-pxSidebarButton.style.background = "#2e2c2c";
-pxSidebarButton.style.marginLeft = "0px";
-pxSidebarButton.style.marginBottom = "0px";
-
-pxSidebarButton.addEventListener('click', (event) => {
-if (pxSidebarOpen) {
-	pxSidebarOpen = !pxSidebarOpen;
-	pxSidebar.style.width = "0%";
-	pxSidebarButton.innerText = "►"
-	pxSidebar.style.transition = "height, 0.1s linear";
-	document.getElementsByClassName("pallete")[0].style.margin = "0px";
-} else {
-	pxSidebarOpen = !pxSidebarOpen;
-	pxSidebar.style.width = "";
-	pxSidebarButton.innerText = "◄";
-	pxSidebar.style.transition = "height, 0.1s linear";
-	document.getElementsByClassName("pallete")[0].style.marginLeft = "";
-
-}
+document.getElementsByClassName("game")[0].appendChild(pxSidebarButton);
+// Svg
+pxSidebarSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+pxSidebarSvg.setAttribute("width", "15");
+pxSidebarSvg.setAttribute("height", "50");
+pxSidebarSvg.style.float = "right";
+pxSidebarSvg.style.color = "gray";
+pxSidebarSvg.style.position = "relative";
+pxSidebarSvg.style.left = "15px";
+pxSidebarSvg.style.zIndex = "999";
+pxSidebarButton.appendChild(pxSidebarSvg);
+// Rect 1
+var pxSidebarRect1 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+pxSidebarRect1.setAttribute("width", "15");
+pxSidebarRect1.setAttribute("height", "50");
+pxSidebarRect1.style.fill = "#2e2c2c";
+pxSidebarRect1.style.rx = "5";
+pxSidebarRect1.style.ry = "5";
+pxSidebarSvg.appendChild(pxSidebarRect1);
+// Rect 2
+var pxSidebarRect2 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+pxSidebarRect2.setAttribute("x", "0");
+pxSidebarRect2.setAttribute("y", "-10");
+pxSidebarRect2.setAttribute("width", "10");
+pxSidebarRect2.setAttribute("height", "100");
+pxSidebarRect2.style.fill = "#2e2c2c";
+pxSidebarRect2.style.rx = "5";
+pxSidebarRect2.style.ry = "5";
+pxSidebarSvg.appendChild(pxSidebarRect2);
+// Triangle
+pxSidebarTriangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+pxSidebarTriangle.setAttribute("points", "10,20 10,30 4,25");
+pxSidebarTriangle.style.fill = "#fff";
+pxSidebarSvg.appendChild(pxSidebarTriangle);
+// Logic
+window.sidebarShown = true;
+pxSidebarSvg.addEventListener('click',function(e){
+  if(window.sidebarShown){
+    pxSidebar.style.left = "-250px";
+    pxPallete.style["margin-left"] = "0";
+    pxSidebarButton.style.left = "-250px";
+    pxSidebarTriangle.setAttribute("points", "4,20 4,30 10,25");
+    window.sidebarShown = false;
+  } else {
+    pxSidebar.style.left = "0";
+    pxPallete.style["margin-left"] = "250px";
+    pxSidebarButton.style.left = "0";
+    pxSidebarTriangle.setAttribute("points", "10,20 10,30 4,25");
+    window.sidebarShown = true;
+  }
 });
-document.body.appendChild(pxSidebarButton);
 
 // --Url Selector Placeholder Text--
 document.getElementById("urlSelector").placeholder = "https://example.com/image.png";
@@ -163,7 +203,7 @@ document.getElementById("urlSelector").placeholder = "https://example.com/image.
 // --Color Hotkeys--
 pxPalleteCheckbox = document.createElement("input");
 pxPalleteCheckbox.type = "checkbox";
-//pxPalleteCheckbox.checked = true;
+pxPalleteCheckbox.checked = true;
 var pxPalleteGroup = document.createElement("form-group");
 pxPalleteGroup.style.display = "block";
 
@@ -172,12 +212,12 @@ window.addEventListener ("keydown", function (e) {
 	if (pxPalleteCheckbox.checked) {
     		if (e.which === 122) {
 			e.preventDefault();
-			if (pxPalleteIndex == 0) { pxPalleteIndex = 29; }
+			if (pxPalleteIndex == 0) { pxPalleteIndex = 28; }
 			else { pxPalleteIndex -= 1; }
 			[...document.getElementsByClassName('btnbelow')][pxPalleteIndex].click();
     		} else if (e.which == 123) {
 			e.preventDefault();
-			if (pxPalleteIndex == 29) { pxPalleteIndex = 0; }
+			if (pxPalleteIndex == 28) { pxPalleteIndex = 0; }
 			else { pxPalleteIndex += 1; }
 			[...document.getElementsByClassName('btnbelow')][pxPalleteIndex].click();
 		}
@@ -199,7 +239,6 @@ pxOpacityGroup.style.display = "block";
 
 var opacityState = true;
 function opacityFade() {
-        //pxOverlay.style.transition = "opacity, 0.5s ease";
         if (opacityState) { pxOverlay.style.opacity = "0.3"; opacityState = false; }
         else { pxOverlay.style.opacity = "0.8"; opacityState = true; }
 }
@@ -220,7 +259,7 @@ pxOpacityGroup.appendChild(pxOpacityCheckbox);
 pxOpacityGroup.appendChild(document.createTextNode("Toggle Opacity Fade"));
 pxOptions.appendChild(pxOpacityGroup);
 
-// --Overlay Comparison--
+// --Overlay Comparison WIP--
 // Values Reference
 // red = pxOverlayPixels[i]
 // green = pxOverlayPixels[i+1]
@@ -228,8 +267,7 @@ pxOptions.appendChild(pxOpacityGroup);
 // alpha = pxOverlayPixels[i+3]
 
 // Defines
-/*var pxCanvas = document.getElementById("myCanvas");
-var pxCanvasCtx = pxCanvas.getContext('2d');
+/*var pxCanvasCtx = pxCanvas.getContext('2d');
 var pxOverlayCanvas = document.createElement("canvas");
 pxOverlayCanvas.width = pxCanvas.width;
 pxOverlayCanvas.height = pxCanvas.height;
